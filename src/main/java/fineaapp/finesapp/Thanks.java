07402e8d417e1c30c3115168115919b4ee550cbb
Alenka <alyonka.piy@gmail.com>
2016-08-14 18:53:29 +0300

    changes 4

---
+++ src/main/java/fineaapp/finesapp/Thanks.java
@@ -0,0 +1,17 @@
+package fineaapp.finesapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Alenka on 14.08.2016.
+ */
+public class Thanks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.thanklayout);
+
+    }
+}
