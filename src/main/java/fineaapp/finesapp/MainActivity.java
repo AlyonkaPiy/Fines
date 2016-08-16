089d661b9a6b30cfc4b8bc1543b7a836bbe66171
Alenka <alyonka.piy@gmail.com>
2016-08-16 14:14:29 +0300

    next com

--- src/main/java/fineaapp/finesapp/MainActivity.java
+++ src/main/java/fineaapp/finesapp/MainActivity.java
@@ -1,14 +1,49 @@
 package fineaapp.finesapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    EditText number_p;
+    EditText money;
+    EditText name;
+    EditText email;
+    Button buttonHelp;
+    Button buttonNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.form);
+
+        number_p = (EditText) findViewById(R.id.number_p);
+        money = (EditText) findViewById(R.id.money);
+        name =(EditText) findViewById(R.id.name);
+        email = (EditText) findViewById(R.id.email);
+
+        buttonHelp = (Button) findViewById(R.id.buttonHelp);
+        buttonHelp.setOnClickListener((View.OnClickListener) this);
+        buttonNext =(Button) findViewById(R.id.buttonNext);
+        buttonNext.setOnClickListener((View.OnClickListener) this);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this, PaiInfo.class);
+        startActivityForResult(intent, 1);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data == null) {return;}
+        String name = data.getStringExtra("name");
+        name.setText();
     }
 }
---
+++ src/main/java/fineaapp/finesapp/PaiInfo.java
@@ -0,0 +1,8 @@
+package fineaapp.finesapp;
+
+/**
+ * Created by Alenka on 16.08.2016.
+ */
+public class PaiInfo {
+
+}
